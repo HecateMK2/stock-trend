c80de9765e10ba553fc66d334bdbff1289255570
hue2 <tohuem@gmail.com>
2020-09-08 17:44:59 -0500

    Added app tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render } from '@testing-library/react';
 import App from '../App';
+import React from 'react';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { StockContext } from '../context/StockContext';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+configure({ adapter: new Adapter() });
+
+describe("App tests", () => {
+    let wrapper
+    beforeEach(() => {
+        wrapper = mount(<App />, {
+            wrappingComponent: StockContext.Provider,
+        });
+    });
+
+    afterAll(() => {
+        wrapper.unmount();
+    });
+
+    it("should render filter", () => {
+        expect(wrapper.find("StockFilter")).toHaveLength(1);
+    });
+
+    it("should render table", () => {
+      expect(wrapper.find("StockTable")).toHaveLength(1);
+    });
+
+    it("should render 3 footer lines", () => {
+      expect(wrapper.find(".footer-text")).toHaveLength(3);
+    });
+})
